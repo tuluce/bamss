4583b16368bc80b071a1b1a4226de17cee0b069a
Bahadır Tülüce <eminbahadir98@gmail.com>
2020-04-11 17:32:28 +0300

    Fix jedis connection timeout

--- bamss/src/main/java/net/bamss/bamss/connections/RedisConnection.java
+++ bamss/src/main/java/net/bamss/bamss/connections/RedisConnection.java
@@ -1,26 +1,30 @@
 package net.bamss.bamss.connections;
 
-import redis.clients.jedis.Jedis;
+import redis.clients.jedis.JedisPool;
+import redis.clients.jedis.JedisPoolConfig;
 
 import java.net.URI;
 import java.net.URISyntaxException;
 
 public class RedisConnection {
-    private static Jedis cache;
+    private static JedisPool jedisPool;
 
     private static void createInstance() throws URISyntaxException {
         URI redisURI = new URI(System.getenv("REDIS_URL"));
-        cache = new Jedis(redisURI);
+        JedisPoolConfig jedisPoolConfig = new JedisPoolConfig();
+        jedisPoolConfig.setMaxIdle(4);
+        jedisPoolConfig.setMaxTotal(8);
+        jedisPool = new JedisPool(jedisPoolConfig, redisURI);
     }
 
-    public static Jedis getRedisCache() {
-        if (cache == null) {
+    public static JedisPool getRedisPool() {
+        if (jedisPool == null) {
             try {
                 createInstance();
             } catch (URISyntaxException e) {
                 e.printStackTrace();
             }
         }
-        return cache;
+        return jedisPool;
     }
 }
--- bamss/src/main/java/net/bamss/bamss/controllers/RedirectController.java
+++ bamss/src/main/java/net/bamss/bamss/controllers/RedirectController.java
@@ -14,6 +14,7 @@ import org.springframework.web.bind.annotation.*;
 import org.springframework.http.ResponseEntity;
 import org.springframework.http.HttpStatus;
 import redis.clients.jedis.Jedis;
+import redis.clients.jedis.JedisPool;
 
 import java.net.URI;
 import java.net.URISyntaxException;
@@ -22,11 +23,13 @@ import java.util.concurrent.CompletableFuture;
 @RestController
 public class RedirectController {
 	private static final MongoDatabase db = MongoConnection.getMongoDatabase();
-	private static final Jedis jedis = RedisConnection.getRedisCache();
+	private static final JedisPool jedisPool = RedisConnection.getRedisPool();
 
 	@GetMapping("/{key}")
 	public ResponseEntity<Object> redirect(@PathVariable String key) throws URISyntaxException {
 		String url = null;
+
+		Jedis jedis = jedisPool.getResource();
 
 		if(jedis != null){
 			url = jedis.get(key);
