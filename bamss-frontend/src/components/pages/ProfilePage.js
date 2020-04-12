0f984c21dcd7841ed85467b2cdd314bbfbf7cc7a
Bahadır Tülüce <eminbahadir98@gmail.com>
2020-04-12 19:26:33 +0300

    Implement url listing in profile

--- bamss-frontend/src/components/pages/ProfilePage.js
+++ bamss-frontend/src/components/pages/ProfilePage.js
@@ -1,37 +1,53 @@
-import React from 'react';
+import React, { Component, Fragment } from 'react';
 import '../../style/App.css';
-import {ListGroup,Button, Row, Container, Col} from 'react-bootstrap';
+import { Button, Card } from 'react-bootstrap';
+import { getSession } from '../../util/session';
+import { CORE_API_ROOT } from '../../util/api_roots';
 
-function ProfilePage() {
-  return (
-    <div className='App'>
-      <header className='App-header'>
-        <Container>
-          <Row xs={2} md={4} lg={6}>
-            <Col>Username: </Col>
-            <Col>Email : </Col>
-          </Row>
-        </Container>
-        <ListGroup >
-          <ListGroup.Item>URL1
-            <Button href='#'>Analytics</Button>
-          </ListGroup.Item>
-          <ListGroup.Item>URL2
-            <Button href='#'>Analytics</Button>
-          </ListGroup.Item>
-          <ListGroup.Item>URL3
-            <Button href='#'>Analytics</Button>
-          </ListGroup.Item>
-          <ListGroup.Item>URL4
-            <Button href='#'>Analytics</Button>
-          </ListGroup.Item>
-          <ListGroup.Item>URL5
-            <Button href='#'>Analytics</Button>
-          </ListGroup.Item>
-        </ListGroup>
-      </header>
-    </div>
-  );
+export default class ProfilePage extends Component {
+  state = { shortUrls: [] }
+
+  async componentDidMount() {
+    const auth_type = getSession().authType;
+    const auth_entity = getSession().authEntity;
+    const response = await fetch(CORE_API_ROOT + '/urls', {
+      method: 'POST',
+      body: JSON.stringify({ [auth_type]: auth_entity }),
+      headers: { 'Content-type': 'application/json; charset=UTF-8' }
+    });
+    if (response.status === 200) {
+      const responseJson = await response.json();
+      this.setState({ shortUrls: responseJson });
+    }
+  }
+
+  getUrlCard(shortUrl, i) {
+    return (
+      <Fragment key={i}>
+        <Card style={{width: "90%"}}>
+          <Card.Header style={{backgroundColor: "grey"}}>
+            https://bamss.herokuapp.com/{shortUrl.key}
+          </Card.Header>
+          <Card.Body  style={{backgroundColor: "white", color: "black"}}>
+            Expiration date: {shortUrl.url}<br/><br/>
+            <Button  variant="dark">Show Analytics</Button>
+          </Card.Body>
+        </Card>
+        <br/>
+      </Fragment>
+    );
+  }
+
+  render() {
+    const urlCards = this.state.shortUrls.map((shortUrl, i) => this.getUrlCard(shortUrl, i));
+    return (
+      <div className='App'>
+        <header className='App-header'>
+          <h1>My URLs (<b>{getSession().username}</b>)</h1>
+          <br/>
+          {urlCards}
+        </header>
+      </div>
+    );
+  }
 }
-
-export default ProfilePage;
