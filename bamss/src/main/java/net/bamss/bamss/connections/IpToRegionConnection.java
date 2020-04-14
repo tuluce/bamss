aa9d51636148d6fd3357a1b89bb83e79d76e6fec
Bahadır Tülüce <eminbahadir98@gmail.com>
2020-04-14 17:11:48 +0300

    Debug region

--- bamss/src/main/java/net/bamss/bamss/connections/IpToRegionConnection.java
+++ bamss/src/main/java/net/bamss/bamss/connections/IpToRegionConnection.java
@@ -7,10 +7,11 @@ public class IpToRegionConnection {
 
   public static String getRegion(String ipAddress) {
     HashMap<String, Object> jsonResponse = HttpConnection.get(IP_TO_REGION_API + "/" + ipAddress);
+    System.out.println("ADDRESS : " + IP_TO_REGION_API + "/" + ipAddress);
+    System.out.println("RESPONSE : " + jsonResponse);
     if (jsonResponse == null || jsonResponse.get("city") == null) {
       return "unknown";
     }
     return (String) jsonResponse.get("city");
   }
-
 }
