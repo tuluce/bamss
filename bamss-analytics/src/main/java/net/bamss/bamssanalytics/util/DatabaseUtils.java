29794df516f3b0c7facf19c7e8baedd0db48577a
Bahadır Tülüce <eminbahadir98@gmail.com>
2020-04-22 23:45:50 +0300

    Fix index error

--- bamss-analytics/src/main/java/net/bamss/bamssanalytics/util/DatabaseUtils.java
+++ bamss-analytics/src/main/java/net/bamss/bamssanalytics/util/DatabaseUtils.java
@@ -146,7 +146,7 @@ public class DatabaseUtils {
   }
 
   private static int getEventsLength(long startDateTs, long endDateTs, long resolution) {
-    return (int) Math.ceil((double) (endDateTs - startDateTs) / resolution) + 1;
+    return (int) Math.ceil((double) (endDateTs - startDateTs) / resolution) + 2;
   }
 
   private static int getEventIndex(Timestamp eventDate, long startDateTs, long resolution) {
